ec9f3ff67d7f67639e01b1cd9e6f5feb1d12a032
NDZD <n_level@mail.ru>
2022-09-03 00:15:06 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// System.Console.WriteLine("Введите пятизначное число: ");
+
+int InputNumber(int fiveDigit){
+
+while (fiveDigit < 10000 || fiveDigit > 99999)
+{
+    System.Console.WriteLine("Введено неверное число!");
+    System.Console.WriteLine("Введите пятизначное число: ");
+    fiveDigit = Convert.ToInt32(Console.ReadLine());
+}
+return fiveDigit;
+}
+string CheckPalindrom(int num){
+    string str;
+if (num / 10000 == num % 10 || (num / 1000 % 10) == (num / 10 % 10) )
+{
+    str = "Палиндром";
+}
+else
+{
+    str = "Не является палиндромом";
+}
+return str;
+}
+System.Console.WriteLine("Введите пятизначное число: ");
+System.Console.WriteLine(CheckPalindrom(InputNumber(Convert.ToInt32(Console.ReadLine()))));
+
