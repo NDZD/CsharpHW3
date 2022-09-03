5bdc48e7b4be592fdee794d53efee439ced0d139
NDZD <n_level@mail.ru>
2022-09-03 07:58:10 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,3 +5,10 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2){
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2)); //(x2-x1,2)+(y2-y1,2)+(z2-z1,2)
+    double finalValue = Math.Round(distance, 2);
+    return finalValue;
+}
+System.Console.WriteLine(GetDistance(3, 6, 8, 2, 1, -7));
