c6856f9598f3c4cb128b1ffe73a7cb34002f7ad6
ndzd <n_level@mail.ru>
2022-09-05 22:09:13 +0300

    fixed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -22,7 +22,7 @@ return fiveDigit;
 }
 string CheckPalindrom(int num){
     string str;
-if (num / 10000 == num % 10 || (num / 1000 % 10) == (num / 10 % 10) )
+if (num / 10000 == num % 10 & (num / 1000 % 10) == (num / 10 % 10) )
 {
     str = "Палиндром";
 }
